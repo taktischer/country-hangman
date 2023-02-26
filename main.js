d04ec8f9a0d20edb4cae0588ae560191fa14300d
Alexander Zotter <91153474+taktischer@users.noreply.github.com>
2023-02-26 03:29:37 +0100

    setup of node.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const path = require("path");
+const app = express();
+
+
+app.set('views', path.join(__dirname, 'src/views'));
+app.use(express.static(path.join(__dirname, 'src/style')));
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+http.createServer(app);
+
+app.get('/', function (req, res){
+    res.render('index');
+});
+
+app.listen(1339);
