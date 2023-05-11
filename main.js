315721d8c52fb399634634c4f133d5a9c2589817
Alexander Zotter <91153474+taktischer@users.noreply.github.com>
2023-05-11 11:59:23 +0200

    documentation (comments)

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
-
+// define path to views and static files
 app.set('views', path.join(__dirname, 'src/views'));
 app.use(express.static(path.join(__dirname, 'src/static')));
 app.engine('.html', require('ejs').__express);
@@ -14,4 +14,6 @@ http.createServer(app);
 app.get('/', function (req, res){
     res.render('index');
 });
+
+// app is listening on port 1339
 app.listen(1339);
--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,26 +1,31 @@
 window["country_hangman"] = window["country_hangman"] || {};
 let CH = window["country_hangman"];
 
+// initialize default settings which are build like this
+// [enabled, effect on score]
+CH["settings"] = {
+    show_flag: [false, -5],               // show the flag at the start of a round
+    show_capital: [false, -5],            // show the capital at the start of a round
+    show_population: [false, -1],         // show the population at the start of a round
+    show_languages: [false, -1],          // show the spoken languages at the start of a round
+    reversed: [false, 10],                // show the country name in reversed
+    helps_first: [false, CH["guesses"]-1] // show helps (flag, capital, population, languages) before revealing letters
+}
+// reveal_rate is defined as the guesses, between every reveal of a letter
+CH["reveal_rate"] = 1;
+
+// initialize global variables
 CH["guesses"] = 0;
 CH["country_data"] = [];
 CH["country_name"] = "";
-CH["reveal_rate"] = 1;
 CH["score"] = 0;
 CH["regions"] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
-CH["settings"] = {
-    show_flag: [false, -5],
-    show_capital: [false, -5],
-    show_population: [false, -1],
-    show_languages: [false, -1],
-    reversed: [false, 10],
-    helps_first: [false, CH["guesses"]-1]
-}
-
 CH["new_country"] = false;
 CH["changed_settings"] = false;
 CH["reveal"] = 0;
 CH["help_level"] = 0;
 
+// definition of functions which refers to elements
 CH.getAnswerField = () => document.getElementById("answer_field");
 CH.getCountryText = () => document.getElementById("country_text");
 CH.getRequiredGuesses = () => document.getElementById("required_guesses");
@@ -35,52 +40,68 @@ CH.getSettingsModal = () =>  document.getElementById('settings_modal');
 CH.getCloseSettings = () => document.getElementsByClassName("close_settings")[0];
 
 CH.getAnswerField().addEventListener('keydown', (e) => {
+    // check if key "enter" is pressed
     if (e.keyCode === 13){
         CH["guesses"]++;
+
+        // check if the answer is correct
         if (CH.getAnswerField().value.toLowerCase().trim() === CH["country_name"].toLowerCase()){
+
+            // calculate the new score
             CH["score"] += 15;
             for (let settingsKey in CH["settings"]) {
                 if (CH["settings"][settingsKey][0]){
                     CH["score"]+=CH["settings"][settingsKey][1];
                 }
             }
-
             CH["score"]-=(CH["guesses"]-1)*2;
+
+            // update the score_info element
             if (CH["score"] > 0){
                 CH.getScoreInfo().textContent = `Score: ${CH["score"]}`;
             } else {
                 CH.getScoreInfo().textContent = "";
             }
-            guessedCountry();
+
+            showCountryInformation();
             return;
         }
+
+        // clear answer_field
         CH.getAnswerField().value = "";
+
+        // check if something should be revealed (reveal_rate)
         if (CH["guesses"]%CH["reveal_rate"] === 0){
+
+            // check if country is already cleared by reveals
             if (CH["reveal"]/CH["reveal_rate"] === CH["country_name"].length-2) {
                 CH["guesses"] = 0;
                 CH["score"] -= 15;
+
+                // update the score_info element
                 if (CH["score"] > 0){
                     CH.getScoreInfo().textContent = `Score: ${CH["score"]}`;
                 } else {
                     CH["score"] = 0;
                     CH.getScoreInfo().textContent = "";
                 }
-                guessedCountry();
+
+                showCountryInformation();
                 return;
             }
-            console.log(CH["help_level"]);
+
             if (CH["settings"]["helps_first"][0] === true && CH["help_level"] !== 4){
                 switch (CH["help_level"]){
-                    case 0:
+                    case 0:     // reveal spoken languages
                         CH.getLanguagesInfo().textContent = `Spoken languages: ${CH["country_data"]['languages']}`;
                         break;
-                    case 1:
+                    case 1:     // reveal population
                         CH.getPopulationInfo().textContent = `Population: ${CH["country_data"]['population']}`;
                         break;
-                    case 2:
+                    case 2:     // reveal capital
                         CH.getCapitalInfo().textContent = `Capital: ${CH["country_data"]['capital']}`;
                         break;
-                    case 3:
+                    case 3:     // reveal flag
                         CH.getFlagImg().src = `${CH["country_data"]['flag']}`;
                         CH.getFlagImg().alt = `${CH["country_name"]}`;
                         break;
@@ -89,6 +110,9 @@ CH.getAnswerField().addEventListener('keydown', (e) => {
             } else {
                 CH["reveal"]+=1;
                 let text_content;
+
+                // update country_name, reveal next letter
+                // example: A__ -> Ab_
                 if (CH["settings"]['reversed'][0]){
                     CH["text_content"] = CH.getCountryText().textContent.substring(0, CH["reveal"]/CH["reveal_rate"]) +
                         CH["country_name"][CH["country_name"].length-1-CH["reveal"]/CH["reveal_rate"]] +
@@ -98,6 +122,8 @@ CH.getAnswerField().addEventListener('keydown', (e) => {
                         CH["country_name"][CH["reveal"]/CH["reveal_rate"]] +
                         CH.getCountryText().textContent.substring(CH["reveal"]/CH["reveal_rate"]+2, CH.getCountryText().length);
                 }
+
+                // update country_text element
                 CH.getCountryText().textContent = text_content;
             }
         }
@@ -105,33 +131,39 @@ CH.getAnswerField().addEventListener('keydown', (e) => {
 })
 
 CH.getSettingsButton().onclick = function (){
+    // show settings modal
     CH.getSettingsModal().style.display = 'block';
 }
 
 CH.getCloseSettings().onclick = function (){
+    // check if something got changed
     if (CH["changed_settings"]){
         getRandomCountry();
     }
     CH["changed_settings"] = false;
+    // hide settings modal
     CH.getSettingsModal().style.display = 'none';
 }
 
 window.onclick = function(event) {
+    // check if something got changed
     if (event.target === CH["settings_modal"]) {
         if (CH["changed_settings"]){
             getRandomCountry();
         }
         CH["changed_settings"] = false;
+        // hide settings modal
         CH.getSettingsModal().style.display = "none";
     }
 }
 
 function getRandomCountry(){
-    console.dir(CH["settings"])
+    // check if country is already a new generated country
     if (CH["new_country"]){
         return;
     }
 
+    // reset country information
     CH.getAnswerField().disabled = false;
     CH.getAnswerField().value = "";
     CH.getCapitalInfo().textContent = "";
@@ -141,6 +173,7 @@ function getRandomCountry(){
     CH.getFlagImg().alt = "";
     CH.getNextCountryBar().hidden = true;
 
+    // send request to https://restcountries.com/v3.1/region/...
     fetch(`https://restcountries.com/v3.1/region/${CH["regions"][Math.floor(CH["regions"].length * Math.random())]}`)
         .then((response) => response.json())
         .then((data) => {
@@ -153,6 +186,8 @@ function getRandomCountry(){
             }
             languages = languages.substring(0, languages.length-2);
             let population = country_obj['population'];
+
+            // make population readable (1.5k, 2.5M)
             if (population/1000 > 1) {
                 population = population/1000;
                 if (population/1000 > 1){
@@ -161,6 +196,7 @@ function getRandomCountry(){
                     population = `${population.toFixed(1)} k`;
                 }
             }
+            // set country_data
             CH["country_data"] = {
                 region: country_obj['region'],
                 capital: country_obj['capital']['0'],
@@ -168,13 +204,17 @@ function getRandomCountry(){
                 population: population,
                 languages: languages
             }
-            CH.getAnswerField().focus();
 
+            CH.getAnswerField().focus();
             let text_content = CH["country_name"][0];
+
+            // reverse country_name if reversed is enabled
             if (CH["settings"]['reversed'][0]){
                 text_content = CH["country_name"][CH["country_name"].length-1];
             }
 
+            // convert country_name to hangman styled text
+            // example: Österreich -> Ö_________
             for (let i = 1; i < CH["country_name"].length; i++){
                 if (CH["country_name"][i] !== " "){
                     if (CH["country_name"][i] === "." || CH["country_name"][i] === "-"){
@@ -186,29 +226,42 @@ function getRandomCountry(){
                     text_content += " ⠀ ";
                 }
             }
+            // update country_text element
             CH.getCountryText().textContent = text_content;
 
+            // show flag if enabled
             if (CH["settings"]['show_flag'][0]){
                 CH.getFlagImg().src = `${CH["country_data"]['flag']}`;
                 CH.getFlagImg().alt = `${CH["country_name"]}`;
             }
+
+            // show capital if enabled
             if (CH["settings"]['show_capital'][0]){
                 CH.getCapitalInfo().textContent = `Capital: ${CH["country_data"]['capital']}`;
             }
+
+            // show population if enabled
             if (CH["settings"]['show_population'][0]){
                 CH.getPopulationInfo().textContent = `Population: ${CH["country_data"]['population']}`;
             }
+
+            // show spoken languages if enabled
             if (CH["settings"]['show_languages'][0]){
                 CH.getLanguagesInfo().textContent = `Spoken languages: ${CH["country_data"]['languages']}`;
             }
 
         })
+    // hide required guesses element
     CH.getRequiredGuesses().hidden = true;
+
+    // reset guesses, help_level, revealed letter counter
     CH["guesses"] = CH["help_level"] = CH["reveal"] = 0;
+
+    // return the generated country_name
     return CH["country_name"];
 }
 
-function guessedCountry(){
+function showCountryInformation(){
     CH.getAnswerField().disabled = true;
     CH.getAnswerField().blur();
     CH.getAnswerField().value = "";
@@ -229,7 +282,7 @@ function guessedCountry(){
         CH.getRequiredGuesses().hidden = false;
     }
     CH["new_country"] = true;
-    CH.getNextCountryBar().hidden = true;//false;
+    CH.getNextCountryBar().hidden = true;
     setTimeout(function () {
         CH["new_country"] = false;
         getRandomCountry();
@@ -237,6 +290,9 @@ function guessedCountry(){
 }
 
 document.querySelector('#settings_checkboxes').onclick = function(element) {
+    // check which settings are checked in the settings_checkboxes
+    // and update those in the CH["settings"]
+
     if (element.target.value.startsWith("show_")){
         CH["settings"][element.target.value][0] = element.target.checked;
         CH["changed_settings"] = true;
@@ -272,14 +328,19 @@ document.querySelector('#settings_checkboxes').onclick = function(element) {
 
 function loadBody(){
     let new_regions = [];
+
+    // add enabled regions to new_regions
     for (let i in CH["regions"]){
         if (document.getElementById(CH["regions"][i].toLowerCase()).checked){
             new_regions.push(CH["regions"][i]);
         }
     }
-    CH["regions"] = new_regions;
-    let show_settings = ["flag", "capital", "population", "languages"];
 
+    // set active regions to the enabled regions (new_regions)
+    CH["regions"] = new_regions;
+
+    // check which settings are enabled and update the CH["settings"]
+    let show_settings = ["flag", "capital", "population", "languages"];
     for (let i in show_settings){
         CH["settings"][`show_${show_settings[i]}`][0] = document.getElementById(`show_${show_settings[i]}`).checked;
     }
